5136eb20f2ebb0f93c1d72bc3c8cffbdfc5cca36
Mic249 <jmika734@gmail.com>
2024-09-20 14:44:02 +0200

    configuration de vercel dans le projet

--- index.js
+++ index.js
@@ -80,6 +80,6 @@ app.post("/test-api", async (req, res) => {
 });
 
 // Start the server on port 3000
-app.listen(3001, () => {
+app.listen(5001, () => {
   console.log("Server is running on port 3000");
 });
