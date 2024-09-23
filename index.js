3551b728e3fc3a07e6c330640d449e964d6273dc
Mic249 <jmika734@gmail.com>
2024-09-23 10:00:04 +0200

    update

--- index.js
+++ index.js
@@ -9,6 +9,34 @@ const OpenAI = require("openai");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// co figuration cors
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+const handler = (req, res) => {
+  const d = new Date();
+  res.end(d.toString());
+};
+
+module.exports = allowCors(handler);
+
 // Configuration CORS pour autoriser tout
 app.use(
   cors({
@@ -32,7 +60,7 @@ const openai = new OpenAI({
 app.get("/", (req, res) => {
   res.json({ message: "Test api v1." });
 });
-app.post("/test-api", async (req, res) => {
+app.post("/test-api", allowCors, async (req, res) => {
   try {
     const { message, latitude, longitude } = req.body;
 
